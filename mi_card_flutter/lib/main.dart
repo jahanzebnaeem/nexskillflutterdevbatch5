091057963ead195a9d158d6a0e020981a6443803
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-01-16 14:40:22 +0500

    Lecture 6
    Complete visiting card design

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -18,35 +18,55 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            // mainAxisSize: MainAxisSize.min,
-            // mainAxisAlignment: MainAxisAlignment.start,
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              const CircleAvatar(
+                backgroundImage: AssetImage("images/jahanzeb.png"),
+                radius: 50.0,
+              ),
+              const Text(
+                "Jahanzeb Naeem",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Pacifico",
+                ),
+              ),
+              Text(
+                "PROJECT MANAGER",
+                style: TextStyle(
+                  fontSize: 22.0,
+                  color: Colors.teal.shade900,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Source Sans Pro",
+                ),
+              ),
               Container(
-                height: 100.0,
-                width: double.infinity,
-                // margin: const EdgeInsets.only(left: 10.0),
-                // padding: const EdgeInsets.all(10.0),
                 color: Colors.white,
-                child: const Text("Container 1"),
-              ),
-              const SizedBox(
-                width: double.infinity,
-              ),
-              Container(
-                height: 100.0,
-                width: double.infinity,
-                // margin: const EdgeInsets.only(left: 10.0),
-                color: Colors.blue,
-                child: const Text("Container 2"),
-              ),
-              Container(
-                height: 100.0,
-                width: double.infinity,
-                // margin: const EdgeInsets.only(left: 10.0),
-                color: Colors.red,
-                child: const Text("Container 3"),
-              ),
+                margin: const EdgeInsets.symmetric(
+                  vertical: 20.0,
+                  horizontal: 15.0,
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "444-444-4444",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 15.0,
+                      ),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
