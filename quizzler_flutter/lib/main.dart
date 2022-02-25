0fa26fd5f8f5fbdcd74b29d344a3318d767943a6
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2022-02-25 21:49:15 +0500

    Lecture 11
    Complete code.

--- quizzler_flutter/lib/main.dart
+++ quizzler_flutter/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'quizzler_brain.dart';
 
-void main() => runApp(Quizzler());
+QuizzlerBrain quizzlerBrain = QuizzlerBrain();
+
+void main() {
+  runApp(Quizzler());
+}
 
 class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
@@ -26,12 +32,15 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-  List<bool> answers = [false, true, true];
+  // List<String> questions = [
+  //   'You can lead a cow down stairs but not up stairs.',
+  //   'Approximately one quarter of human bones are in the feet.',
+  //   'A slug\'s blood is green.'
+  // ];
+  // List<bool> answers = [false, true, true];
+
+  // Question question1 =
+  //     Question('You can lead a cow down stairs but not up stairs.', false);
 
   int originalAnswer = 0;
 
@@ -47,7 +56,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[originalAnswer],
+                quizzlerBrain.checkQuestionText(originalAnswer),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,9 +82,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool selectedAnswer = answers[originalAnswer];
-
-                if (answers[originalAnswer] == true) {
+                bool selectedAnswer =
+                    quizzlerBrain.checkQuestionAns(originalAnswer);
+                if (selectedAnswer == true) {
                   scoreKeeper.add(
                     Icon(
                       Icons.check,
@@ -114,9 +123,10 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool selectedAnswer = answers[originalAnswer];
+                bool selectedAnswer =
+                    quizzlerBrain.checkQuestionAns(originalAnswer);
 
-                if (answers[originalAnswer] == false) {
+                if (selectedAnswer == false) {
                   scoreKeeper.add(
                     Icon(
                       Icons.check,
